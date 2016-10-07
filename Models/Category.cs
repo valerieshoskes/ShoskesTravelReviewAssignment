98a2dcefc611122ca918a7e7fb5ccb653844d36b
valerieshoskes <indi.vs@gmail.com>
2016-10-07 11:18:42 -0400

    initial commit

---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShoskesTravelReviewAssignment.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Models/Review.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ShoskesTravelReviewAssignment.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PublishedDate { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+        public Review()
+        {
+            PublishedDate = DateTime.Now;
+        }
+
+    }
+}
